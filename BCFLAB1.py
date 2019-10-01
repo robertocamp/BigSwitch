2d254365966be6edf6aab66202a2a7cb645455b7
robertocamp <39448916+robertocamp@users.noreply.github.com>
2019-10-01 06:33:54 -0500

    Update BCFLAB1.py

--- BCFLAB1.py
+++ BCFLAB1.py
@@ -1,16 +1,15 @@
 from imp_common.encryption import Encryption
 
 """
-settings file will go to imp_common and retrieve the current API PASSWORD
+settings file will  retrieve the current API password
 """
-
+# the following function is NOT defined in this repo, sorry.
 encrypted = 'gAAAAABdjho8gym25XKZb49H87aN1lLaLIYLhapWMA5bmuu27PcnGNuuAk68G-nDzmmvFKj5ThEKhCGmyda2HCZbVqtVMUI3IA=='
 e = Encryption(env='prod')
 api = e.decrypt(encrypted)
 
 
 POD =  "BCFLAB1"
-#controller_ip = "10.219.0.69"
 URL = "https://10.226.195.224:8443"
 USER = "admin"
 AUTH = api
